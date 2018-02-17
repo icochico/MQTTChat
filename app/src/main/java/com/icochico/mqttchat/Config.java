8b43cbc5446f2db1ba854c373bd7927d5a367a43
Enrico Casini <enrico.casini@gmail.com>
2018-02-17 13:20:44 -0600

    First commit

---
+++ app/src/main/java/com/icochico/mqttchat/Config.java
@@ -0,0 +1,14 @@
+package com.icochico.mqttchat;
+
+/**
+ * Config.java
+ *
+ * Simple interfaces holding connection constants.
+ */
+
+public interface Config {
+
+    String BROKER_URL = "tcp://preprod-mqtt.aira.io:1883";
+    String TOPIC = "chat/Enrico";
+    String CLIENT_ID = "Enrico";
+}
